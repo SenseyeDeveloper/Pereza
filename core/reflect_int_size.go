2b1cd90addfd7248d19ff7284226cc8b3ef50c81
SenseyeDeveloper <senseye.com.ua@gmail.com>
2019-06-19 15:53:38 +0300

    optimize int stubs #3

--- core/reflect_int_size.go
+++ core/reflect_int_size.go
@@ -3,50 +3,83 @@ package core
 import "reflect"
 
 type IntSizeComment struct {
-	Size    int
-	Comment string
+	Size         int
+	SizeAsString string
+	Comment      string
+	TypeCast     bool
+	Signed       bool
 }
 
 var reflectIntSize = map[reflect.Kind]IntSizeComment{
 	reflect.Int: {
-		Size:    20,
-		Comment: "len(`-9223372036854775808`)",
+		Size:         20,
+		SizeAsString: "20",
+		Comment:      "-9223372036854775808",
+		TypeCast:     true,
+		Signed:       true,
 	},
 	reflect.Int8: {
-		Size:    3,
-		Comment: "len(`127`)",
+		Size:         3,
+		SizeAsString: "3",
+		Comment:      "127",
+		TypeCast:     true,
+		Signed:       true,
 	},
 	reflect.Int16: {
-		Size:    5,
-		Comment: "len(`32767`)",
+		Size:         5,
+		SizeAsString: "5",
+		Comment:      "32767",
+		TypeCast:     true,
+		Signed:       true,
 	},
 	reflect.Int32: {
-		Size:    10,
-		Comment: "len(`2147483647`)",
+		Size:         10,
+		SizeAsString: "10",
+		Comment:      "2147483647",
+		TypeCast:     true,
+		Signed:       true,
 	},
 	reflect.Int64: {
-		Size:    20,
-		Comment: "len(`-9223372036854775808`)",
+		Size:         20,
+		SizeAsString: "20",
+		Comment:      "-9223372036854775808",
+		TypeCast:     false,
+		Signed:       true,
 	},
 	reflect.Uint: {
-		Size:    20,
-		Comment: "len(`18446744073709551615`)",
+		Size:         20,
+		SizeAsString: "20",
+		Comment:      "18446744073709551615",
+		TypeCast:     true,
+		Signed:       false,
 	},
 	reflect.Uint8: {
-		Size:    3,
-		Comment: "len(`255`)",
+		Size:         3,
+		SizeAsString: "3",
+		Comment:      "255",
+		TypeCast:     true,
+		Signed:       false,
 	},
 	reflect.Uint16: {
-		Size:    5,
-		Comment: "len(`65535`)",
+		Size:         5,
+		SizeAsString: "5",
+		Comment:      "65535",
+		TypeCast:     true,
+		Signed:       false,
 	},
 	reflect.Uint32: {
-		Size:    10,
-		Comment: "len(`4294967295`)",
+		Size:         10,
+		SizeAsString: "10",
+		Comment:      "4294967295",
+		TypeCast:     true,
+		Signed:       false,
 	},
 	reflect.Uint64: {
-		Size:    20,
-		Comment: "len(`18446744073709551615`)",
+		Size:         20,
+		SizeAsString: "20",
+		Comment:      "18446744073709551615",
+		TypeCast:     false,
+		Signed:       false,
 	},
 }
 
--- pereza/pregenerator/main.go
+++ pereza/pregenerator/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -14,8 +15,11 @@ func main() {
 import "reflect"
 
 type IntSizeComment struct {
-	Size    int
-	Comment string
+	Size         int
+	SizeAsString string
+	Comment      string
+	TypeCast     bool
+	Signed       bool
 }
 
 var reflectIntSize = map[reflect.Kind]IntSizeComment{
@@ -26,32 +30,43 @@ func IntToStringMaxSize(t reflect.Kind) IntSizeComment {
 	return reflectIntSize[t]
 }
 `
-		pattern = "\t%s:{\n\tSize: %d,\n\tComment: \"len(`%s`)\",\n\t},"
+		pattern = "\t%s:{\n\tSize: %d,\n\tSizeAsString: %q,\n\tComment: %q,\n\tTypeCast: %t,\n\tSigned: %t,\n\t},"
 	)
 
 	type reflectSize struct {
 		reflectNameAsKey string
 		intAsString      string
+		typeCast         bool
+		signed           bool
 	}
 
 	// https://stackoverflow.com/questions/16474594/how-can-i-print-out-an-constant-uint64-in-go-using-fmt
 	ints := []reflectSize{
-		{"reflect.Int", fmt.Sprintf("%d", math.MinInt64)},
-		{"reflect.Int8", fmt.Sprintf("%d", math.MaxInt8)},
-		{"reflect.Int16", fmt.Sprintf("%d", math.MaxInt16)},
-		{"reflect.Int32", fmt.Sprintf("%d", math.MaxInt32)},
-		{"reflect.Int64", fmt.Sprintf("%d", math.MinInt64)},
-		{"reflect.Uint", fmt.Sprintf("%d", uint64(math.MaxUint64))},
-		{"reflect.Uint8", fmt.Sprintf("%d", math.MaxUint8)},
-		{"reflect.Uint16", fmt.Sprintf("%d", math.MaxUint16)},
-		{"reflect.Uint32", fmt.Sprintf("%d", math.MaxUint32)},
-		{"reflect.Uint64", fmt.Sprintf("%d", uint64(math.MaxUint64))},
+		{"reflect.Int", fmt.Sprintf("%d", math.MinInt64), true, true},
+		{"reflect.Int8", fmt.Sprintf("%d", math.MaxInt8), true, true},
+		{"reflect.Int16", fmt.Sprintf("%d", math.MaxInt16), true, true},
+		{"reflect.Int32", fmt.Sprintf("%d", math.MaxInt32), true, true},
+		{"reflect.Int64", fmt.Sprintf("%d", math.MinInt64), false, true},
+		{"reflect.Uint", fmt.Sprintf("%d", uint64(math.MaxUint64)), true, false},
+		{"reflect.Uint8", fmt.Sprintf("%d", math.MaxUint8), true, false},
+		{"reflect.Uint16", fmt.Sprintf("%d", math.MaxUint16), true, false},
+		{"reflect.Uint32", fmt.Sprintf("%d", math.MaxUint32), true, false},
+		{"reflect.Uint64", fmt.Sprintf("%d", uint64(math.MaxUint64)), false, false},
 	}
 
 	result := make([]string, len(ints))
 
 	for i, value := range ints {
-		result[i] = fmt.Sprintf(pattern, value.reflectNameAsKey, len(value.intAsString), value.intAsString)
+		size := len(value.intAsString)
+		sizeAsString := strconv.Itoa(size)
+		result[i] = fmt.Sprintf(pattern,
+			value.reflectNameAsKey,
+			size,
+			sizeAsString,
+			value.intAsString,
+			value.typeCast,
+			value.signed,
+		)
 	}
 
 	fmt.Printf(output, strings.Join(result, "\n"))
