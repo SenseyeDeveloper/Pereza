3572613f61d96705e06d5d052994069f17d7069c
SenseyeDeveloper <senseye.com.ua@gmail.com>
2019-06-20 19:10:26 +0300

    optimize json tags parser to common use #5

--- core/tags_parser.go
+++ core/tags_parser.go
@@ -1,40 +1,102 @@
 package core
 
 import (
-	"reflect"
 	"strings"
 )
 
-type fieldTags struct {
-	name string
-
-	omit        bool
-	omitEmpty   bool
-	noOmitEmpty bool
-	asString    bool
-	required    bool
+type FieldTags struct {
+	Name      string
+	Omit      bool
+	OmitEmpty bool
+	AsString  bool
 }
 
-// parseFieldTags parses the json field tag into a structure.
-func parseFieldTags(f reflect.StructField) fieldTags {
-	var ret fieldTags
-
-	for i, s := range strings.Split(f.Tag.Get("json"), ",") {
-		switch {
-		case i == 0 && s == "-":
-			ret.omit = true
-		case i == 0:
-			ret.name = s
-		case s == "omitempty":
-			ret.omitEmpty = true
-		case s == "!omitempty":
-			ret.noOmitEmpty = true
-		case s == "string":
-			ret.asString = true
-		case s == "required":
-			ret.required = true
+// https://golang.org/pkg/encoding/json/
+func ParseFieldTags(json string) FieldTags {
+	switch json {
+	case "":
+		return FieldTags{}
+	case "-":
+		return FieldTags{
+			Omit: true,
+		}
+	case "-,":
+		return FieldTags{
+			Name: "-",
 		}
 	}
 
-	return ret
+	splitIndex := strings.IndexByte(json, ',')
+	if splitIndex == -1 {
+		return FieldTags{
+			Name: json,
+		}
+	}
+
+	settings := strings.Split(json, ",")
+
+	result := FieldTags{}
+	name := settings[0]
+	if name == "-" {
+		result.Omit = true
+	} else {
+		result.Name = name
+	}
+
+	length := len(settings)
+	for i := 1; i < length; i++ {
+		switch settings[i] {
+		case "omitempty":
+			result.OmitEmpty = true
+		case "string":
+			result.AsString = true
+		}
+	}
+
+	return result
 }
+
+func EasyParseFieldTags(json string) FieldTags {
+	result := FieldTags{}
+
+	for i, s := range strings.Split(json, ",") {
+		switch {
+		case i == 0 && s == "-":
+			result.OmitEmpty = true
+		case i == 0:
+			result.Name = s
+		case s == "omitempty":
+			result.OmitEmpty = true
+		case s == "string":
+			result.AsString = true
+		}
+	}
+
+	return result
+}
+
+/**
+// Field appears in JSON as key "myName".
+Field int `json:"myName"`
+
+// Field appears in JSON as key "myName" and
+// the field is omitted from the object if its value is empty,
+// as defined above.
+Field int `json:"myName,omitempty"`
+
+// Field appears in JSON as key "Field" (the default), but
+// the field is skipped if empty.
+// Note the leading comma.
+Field int `json:",omitempty"`
+
+// Field is ignored by this package.
+Field int `json:"-"`
+
+// Field appears in JSON as key "-".
+Field int `json:"-,"`
+
+// The "string" option signals that a field is stored as JSON inside a JSON-encoded string.
+// It applies only to fields of string, floating point, integer, or boolean types.
+// This extra level of encoding is sometimes used when communicating with JavaScript programs
+Int64String int64 `json:",string"`
+*/
---
+++ core/tags_parser_test.go
@@ -0,0 +1,86 @@
+package core
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var parseFieldTagsdataProvider = map[string]FieldTags{
+	"": {},
+	"-": {
+		Omit: true,
+	},
+	"-,": {
+		Name: "-",
+	},
+	"id": {
+		Name: "id",
+	},
+	"value": {
+		Name: "value",
+	},
+	"id,string": {
+		Name:     "id",
+		AsString: true,
+	},
+	"value,string": {
+		Name:     "value",
+		AsString: true,
+	},
+	"id,omitempty": {
+		Name:      "id",
+		OmitEmpty: true,
+	},
+	"value,omitempty": {
+		Name:      "value",
+		OmitEmpty: true,
+	},
+	"id,omitempty,string": {
+		Name:      "id",
+		OmitEmpty: true,
+		AsString:  true,
+	},
+	"value,omitempty,string": {
+		Name:      "value",
+		OmitEmpty: true,
+		AsString:  true,
+	},
+}
+
+func TestParseFieldTags(t *testing.T) {
+	for tags, expect := range parseFieldTagsdataProvider {
+		assert.Equal(t, expect, ParseFieldTags(tags))
+	}
+}
+
+func BenchmarkParseFieldTags(b *testing.B) {
+	n := b.N / len(parseFieldTagsdataProvider)
+
+	for i := 0; i < n; i++ {
+		for tags := range parseFieldTagsdataProvider {
+			_ = ParseFieldTags(tags)
+		}
+	}
+}
+
+func BenchmarkEasyParseFieldTags(b *testing.B) {
+	n := b.N / len(parseFieldTagsdataProvider)
+
+	for i := 0; i < n; i++ {
+		for tags := range parseFieldTagsdataProvider {
+			_ = EasyParseFieldTags(tags)
+		}
+	}
+}
+
+func BenchmarkParseFieldTagsDefault(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = ParseFieldTags("name")
+	}
+}
+
+func BenchmarkEasyParseFieldTagsDefault(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = EasyParseFieldTags("name")
+	}
+}
