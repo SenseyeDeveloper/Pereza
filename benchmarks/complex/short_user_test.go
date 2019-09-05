58a74e1a04f1c02707f3263c690f2c876b0081c1
SenseyeDeveloper <senseye.com.ua@gmail.com>
2019-09-05 17:58:35 +0300

    PerezaShortUser.MarshalJSON test before code

--- benchmarks/complex/short_user_test.go
+++ benchmarks/complex/short_user_test.go
@@ -3,10 +3,53 @@ package complex
 import (
 	"encoding/json"
 	"github.com/gopereza/pereza/fixtures/complex"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func BenchmarkEmptyStateEncodingJSON(b *testing.B) {
+const (
+	expect = `{"id":1,"first_name":"Senseye","last_name":"Developer","country":"UA","created_at":1567695264,"updated_at":1567695265}`
+)
+
+func TestShortUserEncodingJSON(t *testing.T) {
+	source := complex.ShortUser{
+		ID:        1,
+		FirstName: "Senseye",
+		LastName:  "Developer",
+		Country:   "UA",
+		CreatedAt: 1567695264,
+		UpdatedAt: 1567695265,
+	}
+
+	actual, err := json.Marshal(source)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(expect), actual)
+}
+
+func TestShortUserEasyJSON(t *testing.T) {
+	source := complex.EasyShortUser{
+		ID:        1,
+		FirstName: "Senseye",
+		LastName:  "Developer",
+		Country:   "UA",
+		CreatedAt: 1567695264,
+		UpdatedAt: 1567695265,
+	}
+
+	actual, err := source.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(expect), actual)
+}
+
+func TestShortUserPereza(t *testing.T) {
+	source := complex.PerezaShortUser{}
+
+	actual, err := source.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(expect), actual)
+}
+
+func BenchmarkShortUserEncodingJSON(b *testing.B) {
 	source := complex.EasyShortUser{}
 
 	for i := 0; i < b.N; i++ {
@@ -14,7 +57,7 @@ func BenchmarkEmptyStateEncodingJSON(b *testing.B) {
 	}
 }
 
-func BenchmarkEmptyStateEasyJSON(b *testing.B) {
+func BenchmarkShortUserEasyJSON(b *testing.B) {
 	source := complex.EasyShortUser{}
 
 	for i := 0; i < b.N; i++ {
@@ -22,11 +65,10 @@ func BenchmarkEmptyStateEasyJSON(b *testing.B) {
 	}
 }
 
-func BenchmarkEmptyStatePerezaJSON(b *testing.B) {
+func BenchmarkShortUserPerezaJSON(b *testing.B) {
 	source := complex.PerezaShortUser{}
 
 	for i := 0; i < b.N; i++ {
 		_, _ = source.MarshalJSON()
 	}
 }
-
